79e94714f30e6287cb12c93147b9eeb065a82288
Jasmeen241 <jasnoorkaursidhu2002@gmail.com>
2021-12-01 13:09:02 +0530

    adding the functions

--- IOhandler.js
+++ IOhandler.js
@@ -8,6 +8,7 @@
  *
  */
 
+const { resolve } = require("path");
 const unzipper = require("unzipper"),
   fs = require("fs"),
   PNG = require("pngjs").PNG,
@@ -20,7 +21,15 @@ const unzipper = require("unzipper"),
  * @param {string} pathOut
  * @return {promise}
  */
-const unzip = (pathIn, pathOut) => {};
+ const unzip = (pathIn, pathOut) => {
+  return new Promise((resolve, reject) =>{
+      fs.createReadStream(pathIn)
+      .pipe(unzipper.Extract({ path: pathOut }))
+      .on('close', () => resolve())
+      .on('error', (err) => reject(err))
+      
+  })
+};
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
@@ -28,7 +37,18 @@ const unzip = (pathIn, pathOut) => {};
  * @param {string} path
  * @return {promise}
  */
-const readDir = (dir) => {};
+const readDir =  (filename) => {
+  return new Promise((resolve, reject ) => {
+      fs.readdir(filename, (err, files) => {
+          if(err){
+              reject(err)
+          }
+          else{
+              resolve(files)
+          }
+      })
+  })
+}
 
 /**
  * Description: Read in png file by given pathIn,
@@ -38,7 +58,43 @@ const readDir = (dir) => {};
  * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {};
+ const grayScale = (pathIn, pathOut) => {
+  return new Promise((resolve, reject) => {
+   
+  fs.createReadStream(pathIn)
+   .pipe(
+  new PNG({
+    filterType: 4,
+  })
+)
+.on("parsed", function () {
+  for (var y = 0; y < this.height; y++) {
+    for (var x = 0; x < this.width; x++) {
+      var idx = (this.width * y + x) << 2;
+
+      // invert color
+      let avg  =  (this.data[idx] + this.data[idx + 1] +  this.data[idx + 2])/3
+      this.data[idx] = avg;
+      this.data[idx + 1] = avg;
+      this.data[idx + 2] = avg;
+
+      // and reduce opacity
+      this.data[idx + 3] = this.data[idx + 3] >> 1;
+    }
+  }
+  
+
+  this.pack().pipe(fs.createWriteStream(pathOut));
+
+})
+.on('close' ,() => resolve('done'))
+.on("error", (err) => reject(err))
+  
+  })
+
+};
+
+
 
 module.exports = {
   unzip,
